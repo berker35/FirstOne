0365461a83c3ec7c67c8a70ebf533bdb638e341a
berker35 <75068028+berker35@users.noreply.github.com>
2020-11-26 12:11:52 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
+#Multiply function. Takes user input and multiplies.
+
 def my_func():
+    #First number to be multiplied
     num1 = int(input('num1 pls'))
+    #Second number to be multiplied.
     num2 = int(input('num2 pls'))
+    #Returns the result
     return num1 * num2
 my_func()
